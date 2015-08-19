c852e288604aac111e767ca48fe31875a0e9de2f
minhbang <contact@minhbang.com>
2015-08-19 10:56:36 +0700

    Fix Code style

--- src/PackagerNewCommand.php
+++ src/PackagerNewCommand.php
@@ -3,14 +3,13 @@
 namespace JeroenG\Packager;
 
 use Illuminate\Console\Command;
-use JeroenG\Packager\PackagerHelper;
 
 /**
  * Create a brand new package.
  *
  * @package Packager
  * @author JeroenG
- * 
+ *
  **/
 class PackagerNewCommand extends Command
 {
@@ -30,6 +29,7 @@ class PackagerNewCommand extends Command
 
     /**
      * Packager helper class
+     *
      * @var object
      */
     protected $helper;
@@ -37,7 +37,7 @@ class PackagerNewCommand extends Command
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param \JeroenG\Packager\PackagerHelper $helper
      */
     public function __construct(PackagerHelper $helper)
     {
@@ -59,60 +59,81 @@ class PackagerNewCommand extends Command
         // Common variables
         $vendor = $this->argument('vendor');
         $name = $this->argument('name');
-        $path = getcwd().'/packages/';
-        $fullPath = $path.$vendor.'/'.$name;
+        $path = getcwd() . '/packages/';
+        $fullPath = $path . $vendor . '/' . $name;
         $cVendor = ucfirst($vendor);
         $cName = ucfirst($name);
         $requireSupport = '"illuminate/support": "~5.1",
         "php"';
         $requirement = '"psr-4": {
-            "'.$cVendor.'\\\\'.$cName.'\\\\": "packages/'.$vendor.'/'.$name.'/src",';
+            "' . $cVendor . '\\\\' . $cName . '\\\\": "packages/' . $vendor . '/' . $name . '/src",';
         $appConfigLine = 'App\Providers\RouteServiceProvider::class,
 
-        '.ucfirst($vendor).'\\'.ucfirst($name).'\\'.ucfirst($name).'ServiceProvider::class,';
+        ' . ucfirst($vendor) . '\\' . ucfirst($name) . '\\' . ucfirst($name) . 'ServiceProvider::class,';
 
         // Start creating the package        
-        $this->info('Creating package '.$vendor.'\\'.$name.'...');
-            $this->helper->checkExistingPackage($path, $vendor, $name);
+        $this->info('Creating package ' . $vendor . '\\' . $name . '...');
+        $this->helper->checkExistingPackage($path, $vendor, $name);
         $bar->advance();
 
         // Create the package directory
         $this->info('Creating packages directory...');
-            $this->helper->makeDir($path);
+        $this->helper->makeDir($path);
         $bar->advance();
 
         // Create the vendor directory
         $this->info('Creating vendor...');
-         $this->helper->makeDir($path.$vendor);
+        $this->helper->makeDir($path . $vendor);
         $bar->advance();
 
         // Get the skeleton repo from the PHP League
         $this->info('Downloading skeleton...');
-            $this->helper->download($zipFile = $this->helper->makeFilename(), 'http://github.com/thephpleague/skeleton/archive/master.zip')
-                 ->extract($zipFile, $path.$vendor)
-                 ->cleanUp($zipFile);
-            rename($path.$vendor.'/skeleton-master', $fullPath);
+        $this->helper->download(
+            $zipFile = $this->helper->makeFilename(),
+            'http://github.com/thephpleague/skeleton/archive/master.zip'
+        )
+            ->extract($zipFile, $path . $vendor)
+            ->cleanUp($zipFile);
+        rename($path . $vendor . '/skeleton-master', $fullPath);
         $bar->advance();
 
         // Creating a Laravel Service Provider in the src directory
         $this->info('Creating service provider...');
-            $newProvider = $fullPath.'/src/'.ucfirst($name).'ServiceProvider.php';
-            $this->helper->replaceAndSave(__DIR__.'/ServiceProvider.stub', ['{{vendor}}', '{{name}}'], [$cVendor, $cName], $newProvider);
+        $newProvider = $fullPath . '/src/' . ucfirst($name) . 'ServiceProvider.php';
+        $this->helper->replaceAndSave(
+            __DIR__ . '/ServiceProvider.stub',
+            ['{{vendor}}', '{{name}}'],
+            [$cVendor, $cName],
+            $newProvider
+        );
         $bar->advance();
 
         // Replacing skeleton namespaces
         $this->info('Replacing skeleton namespaces...');
-            $this->helper->replaceAndSave($fullPath.'/src/SkeletonClass.php', 'namespace League\Skeleton;', 'namespace '.$cVendor.'\\'.$cName.';');
-            $search = ['league/:package_name',  '"php"',            'League\\\\Skeleton\\\\',       'League\\\\Skeleton\\\\Test\\\\'];
-            $replace = [$vendor.'/'.$name,      $requireSupport,    $cVendor.'\\\\'.$cName.'\\\\',  $cVendor.'\\\\'.$cName.'\\\\Test\\\\'];
-            $this->helper->replaceAndSave($fullPath.'/composer.json', $search, $replace);
+        $this->helper->replaceAndSave(
+            $fullPath . '/src/SkeletonClass.php',
+            'namespace League\Skeleton;',
+            'namespace ' . $cVendor . '\\' . $cName . ';'
+        );
+        $search = ['league/:package_name', '"php"', 'League\\\\Skeleton\\\\', 'League\\\\Skeleton\\\\Test\\\\'];
+        $replace = [
+            $vendor . '/' . $name,
+            $requireSupport,
+            $cVendor . '\\\\' . $cName . '\\\\',
+            $cVendor . '\\\\' . $cName . '\\\\Test\\\\'
+        ];
+        $this->helper->replaceAndSave($fullPath . '/composer.json', $search, $replace);
         $bar->advance();
 
         // Add it to composer.json
         $this->info('Adding package to composer and app...');
-            $this->helper->replaceAndSave(getcwd().'/composer.json', '"psr-4": {', $requirement);
-            // And add it to the providers array in config/app.php
-            $this->helper->replaceAndSave(getcwd().'/config/app.php', 'App\Providers\RouteServiceProvider::class,', $appConfigLine);
+        $this->helper->replaceAndSave(getcwd() . '/composer.json', '"psr-4": {', $requirement);
+        // And add it to the providers array in config/app.php
+        $this->helper->replaceAndSave(
+            getcwd() . '/config/app.php',
+            'App\Providers\RouteServiceProvider::class,',
+            $appConfigLine
+        );
         $bar->advance();
 
         // Finished creating the package, end of the progress bar
